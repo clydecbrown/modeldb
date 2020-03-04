4dbe738ddc62fc971d93497bd858073a215a209a
Michael Liu <miliu@protonmail.com>
2020-03-04 12:07:03 -0800

    Add tests

--- client/verta/tests/test_versioning.py
+++ client/verta/tests/test_versioning.py
@@ -1,45 +1,64 @@
 import pytest
 
 import os
+import subprocess
 import sys
+import tempfile
 
 import six
 
 import utils
 
 import verta.environment
+from verta.environment import _environment_utils
 
 
 pytest.skip("unstable back end support", allow_module_level=True)
 
 
+class TestUtils:
+    def test_parse_pip_freeze(self):
+        req_specs = _environment_utils.get_pip_freeze()
+        parsed_req_specs = (
+            (library, constraint, _environment_utils.parse_version(version))
+            for library, constraint, version
+            in map(_environment_utils.parse_req_spec, req_specs)
+        )
+
+        for library, constraint, parsed_version in parsed_req_specs:
+            assert library != ""
+            assert ' ' not in library
+
+            assert constraint in ('~=', '==', '!=', '<=', '>=', '<', '>', '===')
+
+            assert parsed_version[0] >= 0  # major
+            assert parsed_version[1] >= 0  # minor
+            assert parsed_version[2] >= 0  # patch
+            assert isinstance(parsed_version[2], six.string_types)  # suffix
+
+
 class TestPython:
-    def test_py_ver(self):
+    def test_reqs_from_env(self):
         env = verta.environment.Python()
+        assert env._msg.python.requirements
 
-        assert env._msg.python.version.major == sys.version_info.major
-        assert env._msg.python.version.minor == sys.version_info.minor
-        assert env._msg.python.version.patch == sys.version_info.micro
+    def test_reqs_from_list(self):
+        pytest.importorskip("sklearn")
 
-    def test_env_vars(self):
-        env_vars = os.environ.keys()
-        env = verta.environment.Python(env_vars=env_vars)
+        env = verta.environment.Python(requirements=['sklearn'])
+        req_msg = env._msg.python.requirements[0]
+        assert req_msg.library == "scikit-learn"
+        assert req_msg.constraint == '=='
 
-        assert env._msg.environment_variables
+    def test_reqs_from_file(self):
+        with tempfile.TemporaryFile('w+') as tempf:
+            # create requirements file from pip freeze
+            pip_freeze = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+            pip_freeze = six.ensure_str(pip_freeze)
+            tempf.write(pip_freeze)
+            tempf.flush()  # flush object buffer
+            os.fsync(tempf.fileno())  # flush OS buffer
+            tempf.seek(0)
 
-    def test_commit(self, repository):
-        env = verta.environment.Python()
-
-        commit1 = repository.new_commit()
-        commit1.update('env', env)
-        commit1.save()
-        try:
-            # get from internal blob dict
-            assert commit1.get('env')
-
-            commit2 = repository.new_commit(parents=[commit1])
-
-            # get from back end
-            assert commit2.get('env')
-        finally:
-            utils.delete_commit(repository.id, commit1.id, repository._conn)
+            env = verta.environment.Python(requirements=tempf)
+            assert env._msg.python.requirements
