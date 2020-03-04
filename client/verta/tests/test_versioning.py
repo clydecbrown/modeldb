5decf7df7c5b418591272bd2d1d9c66633d26cf4
Michael Liu <miliu@protonmail.com>
2020-03-04 14:13:51 -0800

    Add tests

--- client/verta/tests/test_versioning.py
+++ client/verta/tests/test_versioning.py
@@ -16,6 +16,39 @@ from verta.environment import _environment_utils
 pytest.skip("unstable back end support", allow_module_level=True)
 
 
+@pytest.fixture
+def requirements_file():
+    with tempfile.TemporaryFile('w+') as tempf:
+        # create constraints file from pip freeze
+        pip_freeze = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+        pip_freeze = six.ensure_str(pip_freeze)
+        tempf.write(pip_freeze)
+        tempf.flush()  # flush object buffer
+        os.fsync(tempf.fileno())  # flush OS buffer
+        tempf.seek(0)
+
+        yield tempf
+
+
+@pytest.fixture
+def requirements_file_without_versions():
+    with tempfile.TemporaryFile('w+') as tempf:
+        # create constraints file from pip freeze
+        pip_freeze = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+        pip_freeze = six.ensure_str(pip_freeze)
+        stripped_pip_freeze = '\n'.join(
+            line.split('==')[0]
+            for line
+            in pip_freeze.splitlines()
+        )
+        tempf.write(stripped_pip_freeze)
+        tempf.flush()  # flush object buffer
+        os.fsync(tempf.fileno())  # flush OS buffer
+        tempf.seek(0)
+
+        yield tempf
+
+
 class TestUtils:
     def test_parse_pip_freeze(self):
         req_specs = _environment_utils.get_pip_freeze()
@@ -50,15 +83,18 @@ class TestPython:
         assert req_msg.library == "scikit-learn"
         assert req_msg.constraint == '=='
 
-    def test_reqs_from_file(self):
-        with tempfile.TemporaryFile('w+') as tempf:
-            # create requirements file from pip freeze
-            pip_freeze = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-            pip_freeze = six.ensure_str(pip_freeze)
-            tempf.write(pip_freeze)
-            tempf.flush()  # flush object buffer
-            os.fsync(tempf.fileno())  # flush OS buffer
-            tempf.seek(0)
+    def test_reqs_from_file(self, requirements_file):
+        env = verta.environment.Python(requirements=requirements_file)
+        assert env._msg.python.requirements
 
-            env = verta.environment.Python(requirements=tempf)
-            assert env._msg.python.requirements
+    def test_reqs_from_file_without_versions(self, requirements_file_without_versions):
+        env = verta.environment.Python(requirements=requirements_file_without_versions)
+        assert env._msg.python.requirements
+
+    def test_constraints_from_file(self, requirements_file):
+        env = verta.environment.Python(constraints=requirements_file)
+        assert env._msg.python.constraints
+
+    def test_constraints_from_file_no_versions_error(self, requirements_file_without_versions):
+        with pytest.raises(ValueError):
+            verta.environment.Python(constraints=requirements_file_without_versions)
